41eaba938c73a1259ef240c0b86d08df799430cd
JohnVindel <jev30_vindel@hotmail.com>
2021-06-16 22:46:35 -0600

    primer commit

---
+++ Semana2-Tarea2-EncontrarAreaCirculo.py
@@ -0,0 +1,9 @@
+#// Calcular el area de un circulo//#
+
+#Declaramos las variables
+import math
+Radio = float(input("Ingrese el radio del circulo: \n"))
+
+Aria = math.pi * (Radio * Radio)
+
+print("El area del circulo es: ", Aria)
